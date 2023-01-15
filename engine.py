200841ed165f7e9eeb3536918c834d64b9041187
WooYung <83138945+TPyun@users.noreply.github.com>
2023-01-15 22:05:51 +0900

    added more roads

--- engine.py
+++ engine.py
@@ -3,9 +3,10 @@ from pygame.locals import *
 import sys  # 외장 모듈
 import math
 
-WIDTH = 1500
-HEIGHT = 1200
+WIDTH = 1920
+HEIGHT = 1080
 FPS = 120
+ZOOM = 1
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -21,11 +22,26 @@ PIXEL_PER_METER = (10.0 / 1.0)  # 10 pixel 1m
 
 
 def key_event():
+    global ZOOM
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             print("quit")
             pygame.quit()
             sys.exit()
+        elif event.type == MOUSEWHEEL:
+            if event.y > 0:
+                if ZOOM < 3:
+                    print("up")
+                    ZOOM += 0.1
+                else:
+                    ZOOM = 3
+            elif event.y < 0:
+                if ZOOM > 1:
+                    print("down")
+                    ZOOM -= 0.1
+                else:
+                    ZOOM = 1
+            print(ZOOM)
 
 
 def get_pixel(length):
@@ -109,6 +125,7 @@ def collide_check(point1, point2, radius):
 
 
 def draw_car():
+    global car_image
     for car_name, car_info in car_dict.items():
         x = car_info['x']
         y = car_info['y']
@@ -117,7 +134,7 @@ def draw_car():
 
         if car_info['crossing'] is True:    # 길이 교차로일 때
             try:
-                if collide_check([x, y], get_road_point(car_info['Route'][car_info['Route'].index(car_info['current_road']) + 1], "start"), 5):
+                if collide_check([x, y], get_road_point(car_info['Route'][car_info['Route'].index(car_info['current_road']) + 1], "start"), 2):
                     car_info['crossing'] = False
                     car_info['current_road'] = car_info['Route'][car_info['Route'].index(car_info['current_road']) + 1]
                     car_info['x'], car_info['y'] = get_road_point(car_info['current_road'], "start")
@@ -151,33 +168,61 @@ def draw_car():
         car_info['y'] = y
 
         color = 0
+        car_width = 1.8
+        car_length = 4.6
         if car_name == 1:
             color = RED
         else:
             color = BLUE
-        pygame.draw.circle(main_display, color, (x, y), 10)
+        pygame.draw.circle(main_display, color, (x, y), get_pixel(car_width / 2))
+
+        # pygame.draw.rect(main_display, color, (x - get_pixel(car_width / 2), y - get_pixel(car_length / 2), get_pixel(car_width), get_pixel(car_length)))
+        # car_image = pygame.transform.scale(car_image, (get_pixel(car_width), get_pixel(car_length)))
+        # car_image = pygame.transform.rotate(car_image, 0.1)
+        #
+        # main_display.blit(car_image, (x - get_pixel(car_width / 2), y - get_pixel(car_length / 2)))
 
 
-road_width = 2
-road_length = 50
+road_width = 3
+road_length = 40
 
 road_dict = {
-    1: {"x": WIDTH/2 - get_pixel(25+2), "y": HEIGHT/2 - 11, "width": road_width, "length": road_length, "direction": "w"},
-    1.1: {"x": WIDTH/2 - get_pixel(25+2), "y": HEIGHT/2 + 11, "width": road_width, "length": road_length, "direction": "e"},
-    2: {"x": WIDTH/2 + get_pixel(25+2), "y": HEIGHT/2 - 11, "width": road_width, "length": road_length, "direction": "w"},
-    2.1: {"x": WIDTH/2 + get_pixel(25+2), "y": HEIGHT/2 + 11, "width": road_width, "length": road_length, "direction": "e"},
+    1: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1, "width": road_width, "length": road_length, "direction": "w"},
+    1.1: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1, "width": road_width, "length": road_length, "direction": "e"},
+    2: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1, "width": road_width, "length": road_length, "direction": "w"},
+    2.1: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1, "width": road_width, "length": road_length, "direction": "e"},
 
-    3: {"x": WIDTH/2 - 11, "y": HEIGHT/2 - get_pixel(25+2), "width": road_width, "length": road_length, "direction": "s"},
-    3.1: {"x": WIDTH/2 + 11, "y": HEIGHT/2 - get_pixel(25+2), "width": road_width, "length": road_length, "direction": "n"},
-    4: {"x": WIDTH/2 - 11, "y": HEIGHT/2 + get_pixel(25+2), "width": road_width, "length": road_length, "direction": "s"},
-    4.1: {"x": WIDTH/2 + 11, "y": HEIGHT/2 + get_pixel(25+2), "width": road_width, "length": road_length, "direction": "n"},
+    3: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1, "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    3.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1, "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+    4: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1, "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    4.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1, "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+
+    5: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1 + get_pixel(road_length + road_width * 2), "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    5.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1 + get_pixel(road_length + road_width * 2), "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+    6: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1 + get_pixel(road_length + road_width * 2), "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    6.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1 + get_pixel(road_length + road_width * 2), "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+
+    7: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1 - get_pixel(road_length + road_width * 2), "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    7.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1 - get_pixel(road_length + road_width * 2), "y": HEIGHT/2 - get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+    8: {"x": WIDTH/2 - get_pixel(road_width)/2 - 1 - get_pixel(road_length + road_width * 2), "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "s"},
+    8.1: {"x": WIDTH/2 + get_pixel(road_width)/2 + 1 - get_pixel(road_length + road_width * 2), "y": HEIGHT/2 + get_pixel(road_length / 2 + road_width), "width": road_width, "length": road_length, "direction": "n"},
+
+    9: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1 - get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "w"},
+    9.1: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1 - get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "e"},
+    10: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1 - get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "w"},
+    10.1: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1 - get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "e"},
+
+    11: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1 + get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "w"},
+    11.1: {"x": WIDTH/2 - get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1 + get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "e"},
+    12: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 - get_pixel(road_width)/2 - 1 + get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "w"},
+    12.1: {"x": WIDTH/2 + get_pixel(road_length / 2 + road_width), "y": HEIGHT/2 + get_pixel(road_width)/2 + 1 + get_pixel(road_length + road_width * 2), "width": road_width, "length": road_length, "direction": "e"},
+
 }
 
 car_dict = {
-    1: {"x": 0.0, "y": 0.0, "crossing": False, "current_road": 0.0, "speed": 0.1, "Route": [4.1, 2.1, 2, 3.1, 3, 4]},
+    1: {"x": 0.0, "y": 0.0, "crossing": False, "current_road": 0.0, "speed": 0.1, "Route": [4.1, 2.1, 5.1, 10, 3, 1, 7.1, 7, 8, 11.1 ]},
     # 2: {"x": 0.0, "y": 0.0, "crossing": False, "current_road": 0.0, "speed": 0.1, "Route": [1.1, 2.1]},
     # 3: {"x": 0.0, "y": 0.0, "crossing": False, "current_road": 0.0, "speed": 0.1, "Route": [2, 1]},
-
 }
 
 
@@ -185,7 +230,11 @@ pygame.init()  # 초기화
 pygame.display.set_caption('Traffic Simulator')
 main_display = pygame.display.set_mode((WIDTH, HEIGHT), 0)
 clock = pygame.time.Clock()  # 시간 설정
+
+car_image = pygame.image.load('car.png')
+
 set_car_start_point()
+
 while True:
     key_event()
     main_display.fill(WHITE)
@@ -194,4 +243,4 @@ while True:
     draw_car()
 
     pygame.display.update()  # 화면을 업데이트한다
-    clock.tick(FPS)  # 화면 표시 회수 설정만큼 루프의 간격을 둔다
+    clock.tick(120)  # 화면 표시 회수 설정만큼 루프의 간격을 둔다
