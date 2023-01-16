d2bf07e370f5320748f5aaed13a95e58ca9681b1
jaeaehun <87132225+jaeaehun@users.noreply.github.com>
2023-01-16 15:24:44 +0900

    Update reinforce.py

--- reinforce.py
+++ reinforce.py
@@ -60,24 +60,41 @@ class ACTOR_CRITIC:
 class project:
 
     def __init__(self):
-        enviroment = engine #게임에서 차량(ego, 상대) 위치 numpy 형태로 부르기
-        model = ACTOR_CRITIC()
-        self.episode_time = 10000
+        self.enviroment = engine #게임에서 차량(ego, 상대) 위치 numpy 형태로 부르기
+        self.model = ACTOR_CRITIC()
+        self.episode_time = 10000 
+        self.print_interval = 10 #에피소드 10번 끝날때마다 리턴값 출력
+        self.score = 0 #리턴
 
     def start(self):
 
-        for n_time in range(self.episode_time): #에피소드가 10000번 실행할때까지
+        for n_time in range(self.episode_time): "에피소드가 10000번 실행할때까지"
 
-            while(#사고가 나기전까지):
-                state = torch.tensor(enviroment) #numpy 형태였던 state를 tensor형태로 바꾸기
+            while("사고가 나기전까지 또는 도착지 도착 전까지"):
+                state = torch.tensor(self.enviroment) "numpy 형태였던 state를 tensor형태로 바꾸기"
 
-                probability = model.policy_network(state) #policy_network에서 가속, 감속을 할 확률을 받음
-                value = model.value_network(state) #상태가치함수를 받음
+                probability = self.model.policy_network(state, 1) "policy_network에서 가속, 감속을 할 확률을 받음"
+                value = self.model.value_network(state) "상태가치함수를 받음"
 
-                Pdf = Categorical(probability) #가감속 할 확률을 확률밀도함수로 만듬
-                action = pdf.sample() #확률밀도함수에서 가속이나 감속을 선택해서 운전함
+                Pdf = Categorical(probability) "가감속 할 확률을 확률밀도함수로 만듬"
+                action = pdf.sample() "확률밀도함수에서 가속이나 감속을 선택하기"
+
+                state, reward, done = engine.move(action.item()) "action을 통해 state와 reward가 변한다. done은 사고, 도착지 도착 true false 반환"
+                next_value = self.model.value_network(state) "변한 state에 대한 상태가치함수 계산"
+
+                self.score += reward
+
+                delta = reward + gamma*next_value - value  "TD 에러 구하기"
+                loss = -torch.log(probability) * delta.item() + delta*delta "loss 계산하기"
+
+                self.model.gather_loss(loss) "loss 모으기"
+
+                if done:
+                    break "사고 나거나 도착지 도착하면 while문 탈출하기"
 
 
+            self.model.train() "위에서 구한 loss로 모델 학습하기"
 
-
-
+            if n_time % self.print_interval == 0 and n_time != 0:
+                print("# of episode :{}, avg score : {:.1f}".format(n_time, score / print_interval))
+                self.score = 0
