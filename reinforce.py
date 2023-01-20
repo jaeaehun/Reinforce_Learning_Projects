507fb6e4f40f4f7a03efedb92d1fa6583cff210c
jaeaehun <87132225+jaeaehun@users.noreply.github.com>
2023-01-20 19:02:17 +0900

    Update reinforce.py

--- reinforce.py
+++ reinforce.py
@@ -2,13 +2,13 @@ import pygame
 from pygame.locals import *
 import sys  # 외장 모듈
 import math
-import random
+
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
+
 from torch.distributions import Categorical
 WIDTH = 800
 HEIGHT = 600
@@ -44,56 +44,64 @@ def select_action(k, s, g, re,o_1, o_2, o_3, o_4):
 
     if luck == 0:
         k.move(1, -1)
-        reward -= 1
+        reward -= 10
 
     elif luck == 1:
         k.move(1, 0)
-        reward -= 1
+        reward -= 10
 
     elif luck == 2:
         k.move(1, 1)
-        reward -= 1
+        reward -= 10
 
     elif luck == 3:
         k.move(0, 1)
-        reward -= 1
+        reward -= 10
 
     elif luck == 4:
         k.move(-1, 1)
-        reward -= 1
+        reward -= 10
 
     elif luck == 5:
         k.move(-1, 0)
-        reward -= 1
+        reward -= 10
 
     elif luck == 6:
         k.move(-1, -1)
-        reward -= 1
+        reward -= 10
 
     else:
         k.move(0, -1)
-        reward -= 1
+        reward -= 10
 
     if collide_check(k, g) == True:
-        reward += 5000
-
-    if collide_check(k, g) == True:
-        reward -= 500
+        print("success")
+        reward += 5
 
     if collide_check(k, o_1) == True:
-        reward -= 500
+        reward -= 50
 
     if collide_check(k, o_2) == True:
-        reward -= 500
+        reward -= 50
 
     if collide_check(k, o_3) == True:
+        reward -= 50
+
+    if collide_check(k, o_4) == True:
+        reward -= 50
+
+    if get_out_check(k) == True:
         reward -= 500
 
-    if get_out_check(o_4) == True:
-        reward -= 10
-        print("fuck")
 
-    return reward
+    done = end_episode()
+
+    #data = [[distance(k, g), distance(k, o_1), distance(k, o_2), distance(k, o_3), distance(k, o_4)]]
+    dx = k.x - g.x
+    dy = k.y - g.y
+    data = [[dx, dy]]
+
+    return data, reward, done
 
 
 class Goal:
@@ -125,7 +133,7 @@ def collide_check(character, goal):
 
 
 def get_out_check(k):
-    if k.x > WIDTH or k.x < 0 or k.y > HEIGHT or k.y < 0:
+    if k.x > WIDTH-50 or k.x < 50 or k.y > HEIGHT-50 or k.y < 50:
         return True
     else:
         return False
@@ -134,28 +142,74 @@ def get_out_check(k):
 def distance(x, y):
     dx = x.x - y.x
     dy = x.y - y.y
+    dis = math.sqrt(dx ** 2 + dy ** 2)
+    return dis
 
-    return dx, dy
+
+def env_reset(k):
+    k.x = 600
+    k.y = 400
+    num = 0
+    score = 0
+    reward = 0
+    #data_1 = [[math.sqrt(500 ** 2 + 300 ** 2), math.sqrt(200 ** 2 + 100 ** 2), math.sqrt(400 ** 2 + 250 ** 2), 200, math.sqrt(150 ** 2 + 250 ** 2)]]
+    data_1 =  [[500, 300]]
+
+    return num, score, reward, data_1
+
+
+def end_episode():
+    clear = collide_check(me, goal)
+    impact = collide_check(me, obstacle)
+    impact_1 = collide_check(me, obstacle_1)
+    impact_2 = collide_check(me, obstacle_2)
+    impact_3 = collide_check(me, obstacle_3)
+    get_out = get_out_check(me)
+    if clear or impact or impact_1 or impact_2 or impact_3 or get_out:
+        return True
+    else:
+        return False
+
+
+def draw_env():
+    me.draw()
+    obstacle.draw()
+    obstacle_1.draw()
+    obstacle_2.draw()
+    obstacle_3.draw()
+    goal.draw()
 
 
 class ActorCritic:
     def __init__(self):
         super(ActorCritic, self).__init__()
 
-        self.loss_lst = []  # loss들을 모아두기 위한 배열
+        self.data = []
 
-        self.fc1 = nn.Linear(2, 128)  # 입력 state 2개
-        self.fc_pi = nn.Linear(128, 8)  # POLICY_NETWORK
-        self.fc_vel = nn.Linear(128, 1)  # value_network
+        self.fc1 = nn.Linear(2, 256)  # 입력 state 2개
+        self.fc_pi = nn.Linear(256, 8)  # POLICY_NETWORK
+        self.fc_vel = nn.Linear(256, 1)  # value_network
 
         model = nn.Linear(1, 1)
 
-        self.optimizer = optim.Adam(model.parameters(), lr = 0.005)  # 딥러닝 최적화 방식
+        self.optimizer = optim.Adam(model.parameters(), lr = 0.001)  # 딥러닝 최적화 방식
 
     def policy_network(self, x, softmax_dim=1):
         x = F.relu(self.fc1(x))
+        #print("x = relu", x)
         x = self.fc_pi(x)
-        act = F.softmax(x, dim=softmax_dim)  # 가속 또는 감속을 할 확률이 나옴
+        #print("x = pi", x)
+        act = F.softmax(x, dim=softmax_dim)  # 8 방향 중에서 어디로 갈지에 대한 확률로 나옴
+        #print("act=", act)
+        return act
+
+    def policy_network_practice(self, x, softmax_dim=-1):
+        x = F.relu(self.fc1(x))
+        #print("x = relu", x)
+        x = self.fc_pi(x)
+        #print("x = pi", x)
+        act = F.softmax(x, dim=softmax_dim)  # 8 방향 중에서 어디로 갈지에 대한 확률로 나옴
+        #print("act=", act)
         return act
 
     def value_network(self, x):
@@ -164,23 +218,91 @@ class ActorCritic:
 
         return value
 
-    def gather_loss(self, loss):
-        self.loss_lst.append(loss.unsqueeze(0))  # loss를 모아둔다. unsqueeze(0)는 차원을 높여주는 것
+    def gather_data(self, transition):
 
-    def train(self):
-        #loss = torch.cat(self.loss_lst).sum
+        self.data.append(transition)
 
-        #print(type(loss_cat))
+    def make_batch(self):
+        s_lst, a_lst, r_lst, s_prime_lst, done_lst = [], [], [], [], []
+        for transition in self.data:
+            s, a, r, s_prime, done = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r/100])
+            s_prime_lst.append(s_prime)
+            done_mask = 0.0 if done else 1.0
+            done_lst.append([done_mask])
 
-        loss = torch.cat(self.loss_lst).mean() #/ len(self.loss_lst)   loss 평균 내주기
+        s_batch, a_batch, r_batch, s_prime_batch, done_batch = torch.tensor(s_lst, dtype=torch.float), torch.tensor(
+            a_lst), \
+                                                               torch.tensor(r_lst, dtype=torch.float), torch.tensor(
+            s_prime_lst, dtype=torch.float), \
+                                                               torch.tensor(done_lst, dtype=torch.float)
+        self.data = []
+        #print("s =", len(s_batch))
+        #print("a_batch =", a_batch)
+        #print("r =", len(r_batch))
+        #print("s_p =", len(s_prime_batch))
+        #print("d =", len(done_batch))
+
+        return s_batch, a_batch, r_batch, s_prime_batch, done_batch
+
+    def train_net(self):
+        s, a, r, s_prime, done = self.make_batch()
+        #print("a=", a)
+        td_target = r + gamma * self.value_network(s_prime) * done
+
+        delta = td_target - self.value_network(s)
+
+        pi = self.policy_network_practice(s).squeeze()
+        #print("pi= ", pi)
+        #print("pi_sum", pi.sum())
+        pi_a = torch.gather(pi, -2, a)
+        #print("pi_a =", pi_a)
+        loss = -torch.log(pi_a) * delta.detach() + delta*delta #F.smooth_l1_loss(self.value_network(s), td_target.detach())
+        #print("loss=", loss)
+        #print("loss_mean=", loss.mean())
 
         self.optimizer.zero_grad()
-        loss.backward()
+        loss.mean().backward()
         self.optimizer.step()
-        self.loss_lst = []
+
+class PolicyGradient(nn.Module):
+    def __init__(self):
+        super(PolicyGradient, self).__init__()
+        self.gama = 0.95
+
+        self.data = []
+
+        self.fc1 = nn.Linear(2, 256)  # 입력 state 2개
+        self.fc2 = nn.Linear(256, 8)  # POLICY_NETWORK
+
+        model = nn.Linear(1, 1)
+
+        self.optimizer = optim.Adam(model.parameters(), lr = 0.0003)  # 딥러닝 최적화 방식
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=1)
+        #print("sum=", x.sum())
+        return x
+
+    def put_data(self, item):
+        self.data.append(item)
+
+    def train_pi(self):
+        R = 0
+        for r, log_prob in self.data[::-1]:
+            R = r + R*self.gama
+            loss = -log_prob*R
+            print("loss =", loss)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+        self.data = []
 
 
-me = Character(600, 400, 20, 10)
+me = Character(600, 400, 20, 8)
 obstacle = Obstacle(400, 300, 20)
 obstacle_1 = Obstacle(200, 150, 20)
 obstacle_2 = Obstacle(600, 200, 20)
@@ -188,88 +310,65 @@ obstacle_3 = Obstacle(450, 150, 20)
 goal = Goal(100, 100, 20)
 
 model = ActorCritic()
+pi = PolicyGradient()
 
 gamma = 0.95
-print(torch.cuda.is_available())
 
 while True:
-    for n in range (1000):
-        clear = False
-        impact = False
-        impact_1 = False
-        impact_2 = False
-        impact_3 = False
-        get_out = False
+    for n in range(10000):
+        num, score, reward, data_1 = env_reset(me)
 
+        while not end_episode() and score > -5000000:
 
-        score = 0
-        reward = 0
-        me.x = 600
-        me.y = 400
-        t=0
-
-        #print("in for")
-
-        #dx, dy = distance(me, goal)
-        data_1 = [[500, 300]]
-
-        while clear or impact or impact_1 or impact_2 or impact_3 or get_out is False:
-
-            clock.tick(60)
+            clock.tick(12000)
             screen.fill((0, 0, 0))
 
-            clear = collide_check(me, goal)
-
-            impact = collide_check(me, obstacle)
-            impact_1 = collide_check(me, obstacle_1)
-            impact_2 = collide_check(me, obstacle_2)
-            impact_3 = collide_check(me, obstacle_3)
-            get_out = get_out_check(me)
-
-            me.draw()
-            obstacle.draw()
-            obstacle_1.draw()
-            obstacle_2.draw()
-            obstacle_3.draw()
-            goal.draw()
+            draw_env()
 
             for event in pygame.event.get():
                 if n == 1000:
                     pygame.quit()
                     sys.exit()
 
-            if me.x == 800 and me.y == 600 and t ==0:
-                    state = torch.tensor(data_1).float()
-                    t += 1
+            if me.x == 600 and me.y == 400 and num == 0:
+                    state = data_1
+                    #probability = model.policy_network(torch.tensor(state).float())
+                    out = pi(torch.tensor(state).float())
+                    num += 1
             else:
-                state = torch.tensor(data).float()
 
-            probability = model.policy_network(state)
-            value = model.value_network(state)
+                state = state_prime
 
-            pdf = Categorical(probability)
-            action = pdf.sample()
-            reward = select_action(me, action.item(), goal, reward, obstacle, obstacle_1, obstacle_2, obstacle_3)
+                #probability = model.policy_network(torch.tensor(state).float())
+                out = pi(torch.tensor(state).float())
 
-            dx, dy = distance(me, goal)
-            data = [[dx, dy]]
 
-            state_prime = torch.tensor(data).float()
-            next_value = model.value_network(state_prime)
-            delta = reward + gamma * next_value - value
-            loss = -torch.log(probability) * delta.item() + delta * delta
+            #pdf = Categorical(probability)
+            m = Categorical(out)
+            act = m.sample()
+            act_1 = act.item()
 
-            model.gather_loss(loss)
+            #action = pdf.sample().item()
+
+           # state_prime, reward, done = select_action(me, action, goal, reward, obstacle, obstacle_1, obstacle_2, obstacle_3)
+            state_prime, reward, done = select_action(me, act_1, goal, reward, obstacle, obstacle_1, obstacle_2,
+                                                      obstacle_3)
+
+            #model.gather_data((state, action, reward, state_prime, done))
+            print("state_size=", torch.tensor(state).float().size())
+            print("out =", out)
+            print("out_size=", out.size())
+            print("act =", act)
+            print("act =", act.size())
+            pi.put_data((reward, torch.log(out[act])))
+
+            #print(torch.log(out[act]))
+
             score += reward
-            # print("score=", score)
-
-            if clear or impact or impact_1 or impact_2 or impact_3 == True:
-
-                break
 
             pygame.display.update()
 
-        model.train()
-
-        if n % 20 == 0 and n != 0:
-            print("# of episode :{}, avg score : {:.1f}".format(n, score / 10))
+        print(n+1, "train_start")
+        #model.train_net()
+        pi.train()
+        print("# of episode :{}, avg score : {:.1f}".format(n, score / 20))
