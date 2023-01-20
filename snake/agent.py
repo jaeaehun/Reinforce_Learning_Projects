842ec9ece06fc1ecd95c1be896ac22d23380c455
WooYung <83138945+TPyun@users.noreply.github.com>
2023-01-20 20:45:51 +0900

    snake game

---
+++ snake/agent.py
@@ -0,0 +1,229 @@
+# PyTorch is an optimized tensor library for deep learning using GPUs and CPUs.
+import torch
+# this library is used to import stack 
+from collections import deque
+# importing the snake_game module
+from snake_game import Nokia_Game, Direction, Point
+# importing from thr model file
+from model import Linear_QNet, QTrainer
+import matplotlib.pyplot as plt
+from IPython import display
+import random
+import numpy as np
+
+plt.ion()
+
+# class Agent
+class Agent:
+    # initializer
+    def __init__(self):
+        # number of games parameter
+        self.num_games = 0
+        # randomness parameter
+        self.randomness = 0 
+        # if stack exceeds memory, it removes elements from the left
+        self.memory = deque(maxlen = 100_000)
+        # the model file's objects are created
+        # Linear_QNet class id called by passing input, hidden and output sizes
+        # Input layer: Size of the state
+        # Output layer: Size of the action
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, learning_rate = 0.001, discount_rate = 0.9)
+
+    # method remember 
+    # inputs: state, new_state, action , reward, game_over
+    def remember(self, state, new_state, action, reward, game_over):
+        # these variables are added to the memory stack as tuples
+        # if the stack if full, it will pop the element to the left
+        self.memory.append((state, action, reward, new_state, game_over))
+
+    # method to get the state
+    # keeps track of the current environment
+    def get_state(self, nokia_game):
+        # head is retrieved from the snake variable in nokia_game class
+        snake_head = nokia_game.snake[0]
+        # point tuples are created in all directions to check if there is any imminent collision 
+        left_point = Point(snake_head.x - 20, snake_head.y)
+        right_point = Point(snake_head.x + 20, snake_head.y)
+        up_point = Point(snake_head.x, snake_head.y - 20)
+        down_point = Point(snake_head.x, snake_head.y + 20)
+        
+        # current direction is a boolean value
+        # it checks the current game direction
+        # only one of the four values will be True
+        current_direction_left = nokia_game.direction == Direction.LEFT
+        current_direction_right = nokia_game.direction == Direction.RIGHT
+        current_direction_up = nokia_game.direction == Direction.UP
+        current_direction_down = nokia_game.direction == Direction.DOWN
+
+        # state has 11 values
+        # [danger straight, danger right, danger left, 
+        # direction_left, direction_right, direction_up, direction_down
+        # worm_left, worm_right, worm_up, worm_down]
+        state = [
+            # checks if the danger is straight or ahead using the above initialized values
+            # this is dependent on current direction
+            (current_direction_right and nokia_game.collision(right_point)) or 
+            (current_direction_left and nokia_game.collision(left_point)) or 
+            (current_direction_up and nokia_game.collision(up_point)) or 
+            (current_direction_down and nokia_game.collision(down_point)),
+
+            # checks if the danger is to the right of the current direction
+            # This is done using the above initialized values
+            # This is dependent on current direction
+            (current_direction_up and nokia_game.collision(right_point)) or 
+            (current_direction_down and nokia_game.collision(left_point)) or 
+            (current_direction_left and nokia_game.collision(up_point)) or 
+            (current_direction_right and nokia_game.collision(down_point)),
+
+            # checks if the danger is to the left of the current direction
+            # This is done using the above initialized values
+            # This is dependent on current direction
+            (current_direction_down and nokia_game.collision(right_point)) or 
+            (current_direction_up and nokia_game.collision(left_point)) or 
+            (current_direction_right and nokia_game.collision(up_point)) or 
+            (current_direction_left and nokia_game.collision(down_point)),
+            
+            # current move direction (only on of the four values are true)
+            current_direction_left,
+            current_direction_right,
+            current_direction_up,
+            current_direction_down,
+            
+            # worm location 
+            # worm is to the left of the snake
+            nokia_game.worm.x < nokia_game.snake_head.x, 
+            # worm is to the right of the snake
+            nokia_game.worm.x > nokia_game.snake_head.x,  
+            # worm is to the up of the snake
+            nokia_game.worm.y < nokia_game.snake_head.y, 
+            # worm is to the down of the snake
+            nokia_game.worm.y > nokia_game.snake_head.y 
+            ]
+        # this array is converted to numpy array of type int and returned
+        return np.array(state, dtype=int)
+
+    # method get_action
+    # get action based on the state
+    def get_action(self, state):
+        # in the beginning, few random moves are needed
+        # This is called a tradeoff between exploration and exploitation
+        # randomness parameter is set
+        # this depends on the number of games
+        # more the games, smaller will be the randomness variable's value
+        self.randomness = 80 - self.num_games
+        # one of the values has to be true
+        move_array = [0,0,0]
+        # if the random value is less than the randomness variable
+        # smaller the value of randomness variable, this if condition's frequency will be less
+        if random.randint(0, 200) < self.randomness:
+            # it chooses a random move (either straight, left or right)
+            index = random.randint(0, 2)
+            # move_array is updated to that direction
+            move_array[index] = 1
+        else:
+            # here, move based on the model is done
+            # this state is converted to tensor of float type
+            first_state = torch.tensor(state, dtype=torch.float)
+            # it wants to predict action based on the first_state
+            # prediciton has raw float values
+            prediction = self.model(first_state)
+            # this gets the maximum value from the tensor
+            idx = torch.argmax(prediction).item()
+            # sets the maximum index value to 1, thereby changing the direction
+            move_array[idx] = 1
+        # new move direction is returned
+        return move_array
+
+    def long_memory_train(self):
+        # if the memory size is greater than the batch size
+        if len(self.memory) > 1000:
+            # random samples of SIZE_BATCH is stored in random_smaller_sample
+            random_smaller_sample = random.sample(self.memory, 1000)
+        else:
+            # else, the entire memory is stored in random_smaller_sample
+            random_smaller_sample = self.memory
+        # trainer is called to do the optimization
+        for current_state, action, reward, new_state, game_over in random_smaller_sample:
+            self.trainer.train_step(current_state, action, reward, new_state, game_over)
+
+    def short_memory_train(self, current_state, action, reward, new_state, game_over):
+        # trainer is called to do the optimization
+        self.trainer.train_step(current_state, action, reward, new_state, game_over)
+
+# this method is used to plot the scores in a graph
+def plot(scores, mean_scores):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(.1)
+
+def train_model():
+    # keeps track of scores to plot
+    score_graph = []
+    # mean of the score to plot 
+    mean_score_graph = []
+    # keeps track of the total score
+    score_sum = 0
+    # keeps track of the best score
+    best_score = 0
+    #  creating a agent object
+    agent = Agent()
+    # creating a Nokia_Game object
+    nokia_game = Nokia_Game()
+    # training loop
+    while True:
+        # getting current state by calling the get_state method
+        current_state = agent.get_state(nokia_game)
+        # getting move based on current state
+        move = agent.get_action(current_state)
+        # this move is used to play the game
+        # the values obtained from the move is used to get a new state
+        reward, game_over, score = nokia_game.play_game(move)
+        new_state = agent.get_state(nokia_game)
+        # to train short memory - 
+        agent.short_memory_train(current_state, move, reward, new_state, game_over)
+        # to remember the current situation
+        agent.remember(current_state, new_state, move, reward, game_over)
+
+        # if the game is over
+        if game_over:
+            # initializes game and resets everything 
+            nokia_game.reset_game()
+            # increments the num_games by 1 at the start of new game
+            agent.num_games += 1
+            # to train long memory - replay memory or experienced replay
+            # it trains on all previous moves and games. Temendously improves the model
+            agent.long_memory_train()
+
+            # updates best_score if there is a new high score
+            if score > best_score:
+                best_score = score
+                # if there is a new high score, this model is saved
+                agent.model.save_model()
+
+            # printing all the paramters
+            print('Game', agent.num_games, 'Score', score, 'Best Score:', best_score)
+
+            # the current score is added to the list of scores to plot in the graph
+            score_graph.append(score)
+            # current score is added to total score_sum 
+            score_sum += score
+            # mean score is calculated
+            mean_score = score_sum / agent.num_games
+            # mean score is appended to the list
+            mean_score_graph.append(mean_score)
+            # these values are then plotted
+            plot(score_graph, mean_score_graph)
+
+if __name__ == '__main__':
+    train_model()
---
+++ snake/snake_game.py
@@ -0,0 +1,239 @@
+from collections import namedtuple
+import pygame as pg
+from enum import Enum
+import random as rand
+import numpy as np
+pg.init()
+
+# setting the font_face and size
+font_style = pg.font.SysFont("cambria", 25)
+
+# setting the values for each of the direction using Enum
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+# colors defined
+neon = (204, 255, 102)
+dark_green = (0, 51, 0)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+# snake and its speed is set
+snake_block = 20
+speed = 15
+
+Point = namedtuple('Point', 'x, y')
+
+
+# created a class Nokia_Game
+class Nokia_Game:
+    # initializer has parameters width (default: 600) and height (default: 400)
+    def __init__(self, width = 600, height = 400):
+        # class fields
+        self.width = width
+        self.height = height
+        # displays the window using the pygame library with width and height
+        self.window = pg.display.set_mode((self.width, self.height))
+        self.clock = pg.time.Clock()
+        # title of the window is set
+        pg.display.set_caption('Nokia Snake Game')
+
+        # reset_game method is called
+        self.reset_game()
+
+
+    def reset_game(self):
+        # initial direction is set
+        self.direction = Direction.RIGHT
+
+        self.snake_head = Point(self.width // 2, self.height // 2)
+        self.snake = [self.snake_head,
+                      Point(self.snake_head.x - snake_block, self.snake_head.y),
+                      Point(self.snake_head.x - (2 * snake_block), self.snake_head.y)]
+        # initial score is set to 0
+        self.score = 0
+        # initial value for the worm is set to None
+        self.worm = None
+        # insert worm method is called to set the x and y coordinates 
+        self.insert_worm()
+        # keeps track of the game iteration
+        self.game_iteration = 0
+
+
+    def insert_worm(self):
+        # worm's x and y coordiantes are set and assigns random values within the frame
+        worm_x = rand.randint(0, (self.width - snake_block) // snake_block) * snake_block
+        worm_y = rand.randint(0, (self.height - snake_block) // snake_block) * snake_block
+        self.worm = Point(worm_x, worm_y)
+        # if the worm is inserted at the snake's position
+        if self.worm in self.snake:
+            # the insert_worm method is called again to insert the worm ata different position
+            self.insert_worm()
+
+
+    def play_game(self, action):
+        # game iteration is incremented
+        self.game_iteration += 1
+        # gets user input
+        for event in pg.event.get():
+            # checks if the event is quit, then pg.quit() and sys.exit() methods are called
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+            # # checks if any key is pressed down
+            # if event.type == pg.KEYDOWN:
+            #     # checks if the key pressed is up arrow
+            #     if event.key == pg.K_UP:
+            #         # value 3 is set to the variable direction
+            #         self.direction = Direction.UP
+            #     # checks if the key pressed is down arrow
+            #     elif event.key == pg.K_DOWN:
+            #         # value 4 is set to the variable direction
+            #         self.direction = Direction.DOWN
+            #     # checks if the key pressed is left arrow
+            #     elif event.key == pg.K_LEFT:
+            #         # value 1 is set to the variable direction
+            #         self.direction = Direction.LEFT
+            #     # checks if the key pressed is right arrow
+            #     elif event.key == pg.K_RIGHT:
+            #         # value 2 is set to the variable direction
+            #         self.direction = Direction.RIGHT
+
+        # depending on the value in action, the direction of the snake is changed
+        # by calling by the move_direction method
+        # This updates the head
+        self.move_direction(action)
+        # the updated head is added to the snake
+        self.snake.insert(0, self.snake_head)
+
+        # to check reward, the value is initialized to 0
+        reward = 0
+        # game_over variable is set to False
+        game_over = False
+        # check if the game is over by calling the collision method
+        # or if there are not a lot of changes after many iterations
+        if self.collision() or self.game_iteration > 100*len(self.snake):
+            # sets the game_over variable to True
+            game_over = True
+            # if the game is over, the reward is set to -100
+            reward = -100
+            # returns the game_over and score variable to the main method
+            return reward, game_over, self.score
+
+        # if the head of the snake touches the worm
+        if self.snake_head == self.worm:
+            # if snake eats the food, reward is set to 100
+            reward = 100
+            # score is incremented by 1
+            self.score += 1
+            # the worm is inserted elsewhere by calling the insert_worm method
+            self.insert_worm()
+        else:
+            self.snake.pop()
+
+        # updating interface by calling the method interface_update
+        self.interface_update()
+        # updating the clock speed 
+        self.clock.tick(speed)
+        # returning the values in game_over and score variable
+        return reward, game_over, self.score
+
+
+    # checks if any collision has happened
+    def collision(self, point = None):
+        if point is None:
+            point = self.snake_head
+        # if the head of the snake hits any of the boundaries, it will return a boolean value 'True'
+        if (point.x > self.width - snake_block) or (point.x < 0) or (point.y > self.height - snake_block) or (point.y < 0):
+            return True
+        # if the head of the snake hit any part of itself, it will return a boolean value 'True'
+        if point in self.snake[1:]:
+            return True
+        # if it does not satisfy any of the above conditions, it will return a boolean value 'False'
+        return False
+
+
+    # function to display message in the frame
+    def display_message(self, message, font_color, coordinates):
+        # Text antialiasing (set to False) is a technique used to smooth the edges of text on a screen.
+        message = font_style.render(message, False, font_color)
+        # message at coordinates (0,0) is displayed
+        self.window.blit(message, coordinates)
+
+
+    def interface_update(self):
+        # the background display is filled with neon color
+        self.window.fill(neon)
+
+        # snake is drawn in the window
+        for eachPoint in self.snake:
+            pg.draw.rect(self.window, dark_green, pg.Rect(eachPoint.x, eachPoint.y, snake_block, snake_block))
+            pg.draw.rect(self.window, dark_green, pg.Rect(eachPoint.x+4, eachPoint.y+4, 12, 12))
+
+        # worm's is drawn in the window with color and coordinates
+        pg.draw.rect(self.window, red, pg.Rect(self.worm.x, self.worm.y, snake_block, snake_block))
+        # score is updated by calling the method display_message
+        self.display_message("Score: " + str(self.score), black, [0, 0])
+        pg.display.flip()
+
+
+   # this method is used to determine the direction based on the action
+    def move_direction(self, action):
+        # Index 0: Straight
+        # Index 1: Right
+        # Index 2: Left
+        clockwise_directions = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        index = clockwise_directions.index(self.direction)
+        # if action is equal to [1,0,0], no change in direction
+        if np.array_equal(action, [1,0,0]):
+            new_direction = clockwise_directions[index]
+        # if action is equal to [0,1,0]
+        # right -> down -> left -> up
+        elif np.array_equal(action, [0,1,0]):
+            # it moves to the next value in the direction_arr
+            new_index = (index + 1) % 4
+            new_direction = clockwise_directions[new_index]
+
+        # if action is equal to [0,0,1]
+        # right -> up -> left -> down
+        else:
+            # it moves to the previous value in the direction_arr
+            new_index = (index - 1) % 4
+            new_direction = clockwise_directions[new_index]
+
+        # direction is changed to updated value
+        self.direction = new_direction
+
+        # the x and y axis values are set
+        x_axis = self.snake_head.x
+        y_axis = self.snake_head.y
+        # depending on the direction of the snake, x and y axis values are changed
+        if self.direction == Direction.DOWN:
+            y_axis += snake_block
+        elif self.direction == Direction.UP:
+            y_axis -= snake_block
+        elif self.direction == Direction.RIGHT:
+            x_axis += snake_block
+        elif self.direction == Direction.LEFT:
+            x_axis -= snake_block
+        # the head value is updated
+        self.snake_head = Point(x_axis, y_axis)
+
+
+# if __name__ == '__main__':
+#     # nokia_game is created with class Nokia_Game
+#     nokia_game = Nokia_Game()
+#     # this loop will run till the game_over variable is set to True
+#     while True:
+#         # values returned from the play_game method is stored in game_over and score
+#         game_over, score = nokia_game.play_game()
+#         # if the game is over
+#         if game_over:
+#             break
+#     # score is printed
+#     print('Score', score)
+#     # the game window is closed
+#     pg.quit()
